1832eb686a466206e0484cc1ae2f4e4dabf216f7
Anna JEARUTH <ajearuth@student.42.fr>
2022-05-24 11:52:46 +0200

    bonus compiling

--- bonus/sources/events_bonus.c
+++ bonus/sources/events_bonus.c
@@ -3,14 +3,71 @@
 /*                                                        :::      ::::::::   */
 /*   events_bonus.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smagdela <smagdela@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ajearuth <ajearuth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 14:51:40 by smagdela          #+#    #+#             */
-/*   Updated: 2022/05/23 14:52:22 by smagdela         ###   ########.fr       */
+/*   Updated: 2022/05/24 11:30:47 by ajearuth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D_bonus.h"
+
+int	loop_handler(t_data *data)
+{
+	if (data->render)
+	{
+		raycast_renderer(data);
+		mlx_put_image_to_window(data->win->mlx_ptr, data->win->win_ptr,
+			data->pov->img_ptr, 0, 0);
+		data->render = 0;
+	}
+	move_player(data);
+	return (0);
+}
+
+int	red_cross_handler(t_data *data)
+{
+	free_n_destroy(data);
+	ft_putstr_fd("Closing...\n\033[0;32mThanks for using cub3D!\033[0m\n", 1);
+	exit(EXIT_SUCCESS);
+	return (0);
+}
+
+int	keys_press(int key_sym, t_data *data)
+{
+	if (key_sym == XK_Escape)
+		red_cross_handler(data);
+	else if (key_sym == XK_w)
+		data->forward = 1;
+	else if (key_sym == XK_d)
+		data->right = 1;
+	else if (key_sym == XK_a)
+		data->left = 1;
+	else if (key_sym == XK_s)
+		data->backward = 1;
+	else if (key_sym == XK_Left)
+		data->rot_left = 1;
+	else if (key_sym == XK_Right)
+		data->rot_right = 1;
+	return (0);
+}
+
+int	keys_release(int key_sym, t_data *data)
+{
+	if (key_sym == XK_w)
+		data->forward = 0;
+	else if (key_sym == XK_d)
+		data->right = 0;
+	else if (key_sym == XK_a)
+		data->left = 0;
+	else if (key_sym == XK_s)
+		data->backward = 0;
+	else if (key_sym == XK_Left)
+		data->rot_left = 0;
+	else if (key_sym == XK_Right)
+		data->rot_right = 0;
+	return (0);
+}
 
 /*
 cub2D, minimap renderer
