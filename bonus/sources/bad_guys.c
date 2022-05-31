3d5a3279b420b97116db37149771a47e862551f9
smagdela <smagdela@student.42.fr>
2022-05-31 17:42:08 +0200

    Sprites implemented! Two fix remains to be done : offset of sprites when on the left screen limit and handling the rendering of further sprites first

--- bonus/sources/bad_guys.c
+++ bonus/sources/bad_guys.c
@@ -6,42 +6,52 @@
 /*   By: smagdela <smagdela@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 15:25:09 by smagdela          #+#    #+#             */
-/*   Updated: 2022/05/31 16:07:43 by smagdela         ###   ########.fr       */
+/*   Updated: 2022/05/31 17:02:19 by smagdela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D_bonus.h"
 
-void	put_sprite_to_pov(t_data *data, t_point sprite, t_point transform)
+void	put_sprite_to_pov(t_data *data, t_point transform)
 {
 	t_point	start;
 	t_point	stop;
-	t_point	pixel;
+	t_point	scrn_pix;
+	t_point	tex_pix;
 	int		sprite_dim;
+	int		color;
 
-	sprite_dim = (int)abs(HEIGHT / transform.y);
+	sprite_dim = (int)fabs(HEIGHT * TEXTURE_DIM / transform.y);
 	start.y = -sprite_dim / 2 + HEIGHT / 2;
 	if (start.y < 0)
 		start.y = 0;
 	stop.y = sprite_dim / 2 + HEIGHT / 2;
 	if (stop.y >= HEIGHT)
 		stop.y = HEIGHT - 1;
-	start.x = -sprite_dim / 2 + (WIDTH / 2) * (1 + transform.x / transform.y);
+	start.x = ((WIDTH / 2) * (1 + transform.x / transform.y)) - (sprite_dim / 2);
 	if (start.x < 0)
 		start.x = 0;
-	stop.x = sprite_dim / 2 + (WIDTH / 2) * (1 + transform.x / transform.y);
+	stop.x = (sprite_dim / 2) + ((WIDTH / 2) * (1 + transform.x / transform.y));
 	if (stop.x >= WIDTH)
 		stop.x = WIDTH - 1;
-	pixel.x = start.x;
-	while (pixel.x < stop.x && pixel.x >= 0 && pixel.x < WIDTH)
+	tex_pix.x = 0;
+	scrn_pix.x = start.x;
+	while (scrn_pix.x < stop.x && scrn_pix.x >= 0 && scrn_pix.x < WIDTH)
 	{
-		pixel.y = start.y;
-		while (pixel.y < stop.y && pixel.y >=0 &&pixel.y < HEIGHT
-			&& transform.y > 0 && transform.y < data->dist[pixel.x])
+		tex_pix.y = 0;
+		scrn_pix.y = start.y;
+		while (scrn_pix.y < stop.y && scrn_pix.y >=0 && scrn_pix.y < HEIGHT
+			&& transform.y > 0 && transform.y < data->dist[(int)scrn_pix.x])
 		{
-			++pixel.y;
+			color = get_pixel_color(trunc(tex_pix.x),
+					trunc(tex_pix.y), data->texture);
+			if ((color & 0xff000000) == 0)
+				draw_pixel(data->pov, scrn_pix.x, scrn_pix.y, color);
+			++scrn_pix.y;
+			tex_pix.y += TEXTURE_DIM / sprite_dim;
 		}
-		++pixel.x;
+		tex_pix.x += TEXTURE_DIM / sprite_dim;
+		++scrn_pix.x;
 	}
 }
 
@@ -78,7 +88,6 @@ void	put_sprite_to_pov(t_data *data, t_point sprite, t_point transform)
 void	render_mobs(t_data *data)
 {
 	t_mob	*mob;
-	t_point	sprite;
 	t_point	transform;
 	t_point	plane;
 
@@ -89,21 +98,21 @@ void	render_mobs(t_data *data)
 			mob = mob->next;
 		else
 		{
-			sprite.x = mob->pos_x - data->player_x;
-			sprite.y = mob->pos_y - data->player_y;
 			plane.x = cos(data->player_orient - M_PI_2)
 				* tan(FOV * M_PI / 360);
 			plane.y = -sin(data->player_orient - M_PI_2)
 				* tan(FOV * M_PI / 360);
-			transform.x = invDet * (-sin(data->player_orient) * sprite.x
-					- cos(data->player_orient) * sprite.y) / (plane.x
-					* -sin(data->player_orient)
+			transform.x = (-sin(data->player_orient)
+					* (mob->pos_x - data->player_x)
+					- cos(data->player_orient) * (mob->pos_y - data->player_y))
+				/ (plane.x * -sin(data->player_orient)
 					- cos(data->player_orient) * plane.y);
-			transform.y = invDet * (-plane.y * sprite.x
-					+ plane.x * sprite.y) / (plane.x * -sin(data->player_orient)
+			transform.y = (-plane.y * (mob->pos_x - data->player_x)
+					+ plane.x * (mob->pos_y - data->player_y))
+				/ (plane.x * -sin(data->player_orient)
 					- cos(data->player_orient) * plane.y);
 			data->texture = mob->mob1;
-			put_sprite_to_pov(data, sprite, transform);
+			put_sprite_to_pov(data, transform);
 			mob = mob->next;
 		}
 	}
